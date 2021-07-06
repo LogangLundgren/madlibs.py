2e289ec692aa61b6db9f880215bf16e690f1cdf4
LogangLundgren <86979324+LogangLundgren@users.noreply.github.com>
2021-07-05 21:29:15 -0400

    Madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+name =input("enter a name: ")
+adjectiveOne =input("Enter an adjective: ")
+adjectiveTwo =input("Enter another adjective: ")
+adverb =input("Enter an adverb: ")
+foodOne =input("Enter a food: ")
+foodTwo = input("Enter another food: ")
+noun =input("Enter a noun: ")
+place =input("Enter a place: ")
+verb =input("Enter a verb: ")
+print(f"""
+   {name} was planning a dream vacation to {place}.
+{name} was especially looking forward to trying the local
+cuisine, including {adjectiveOne} {foodOne} and {adjectiveTwo} {foodTwo}.
+ 
+{name} will have to practice the language {adverb} to
+make it easier to {verb} with people.
+ 
+{name} has a long list of sights to see, including the
+{noun} museum and the {adjectiveOne} river.
+""")
